0279e829844bbf23f45c2c1a3d67f9ae40dc1eab
callumpoole <callumpoole06@hotmail.co.uk>
2020-07-04 10:21:05 +0700

    Moved gl window code out of foo and into a proper class

---
+++ src/Poole.cpp
@@ -0,0 +1,36 @@
+#include "Poole.h"
+
+PooleEngine::PooleEngine(const char* windowName, int width, int height)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(width, height, windowName, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
