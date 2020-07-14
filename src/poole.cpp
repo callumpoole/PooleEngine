ac527a313fc64b748686c0d9db89c69464be4d5a
callumpoole <callumpoole06@hotmail.co.uk>
2020-07-14 17:12:47 +0100

    Cleanup CMake, case of file names, comments

--- src/poole.cpp
+++ src/poole.cpp
@@ -1,9 +1,8 @@
-#include "Poole.h"
-//Testing
+#include "poole.h"
 #include <iostream>
 
-// This will identify our vertex buffer
-GLuint vertexbuffer;
+//This will identify our vertex buffer
+GLuint vertexbuffer; //TEMP
 
 void TestInit()
 {
@@ -20,29 +19,29 @@ void TestInit()
 
 
 
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    //Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
+    //The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
+    //Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 }
 
 void TestTriangle()
 {
-    // 1st attribute buffer : vertices
+    //1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(
-        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-        3,                  // size
-        GL_FLOAT,           // type
-        GL_FALSE,           // normalized?
-        0,                  // stride
-        (void*)0            // array buffer offset
+        0,                  //attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  //size
+        GL_FLOAT,           //type
+        GL_FALSE,           //normalized?
+        0,                  //stride
+        (void*)0            //array buffer offset
     );
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+    //Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); //Starting from vertex 0; 3 vertices total -> 1 triangle
     glDisableVertexAttribArray(0);
 }
 
@@ -50,43 +49,48 @@ PooleEngine::PooleEngine(const char* windowName, glm::uvec2 size)
 {    
     GLFWwindow* window;
 
-    /* Initialize the library */
+    //Initialize the library GLFW
     if (!glfwInit())
+    {
+        std::cout << "Failed to Init GLFW" << std::endl;
         return;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
+    //Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(size.x, size.y, windowName, NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to Create Window" << std::endl;
         glfwTerminate();
         return;
     }
-    /* Make the window's context current */
+    //Make the window's context current
     glfwMakeContextCurrent(window);
 
-
+    //Init Glad
     if (!gladLoadGL()) {
         std::cout << "Failed to initialize OpenGL context" << std::endl;
-        exit(1010);
+        return;
     }
-
-    if (GLAD_GL_VERSION_3_0 == 0) {
-        exit(1337);
+    if (!GLAD_GL_VERSION_3_0)
+    {
+        std::cout << "Open GL version too low!" << std::endl;
+        return;
     }
 
     TestInit();
 
-    /* Loop until the user closes the window */
+    //Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        //Render here
         glClear(GL_COLOR_BUFFER_BIT);
         TestTriangle();
 
-        /* Swap front and back buffers */
+        //Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        //Poll for and process events
         glfwPollEvents();
     }
 
