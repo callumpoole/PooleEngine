68c644c65f282512f91d8c534746ee181da408d8
callumpoole <14567682+callumpoole@users.noreply.github.com>
2021-10-11 23:40:36 +0100

    Further Progress towards better uniforms

--- src/rendering/rendering_primitives.cpp
+++ src/rendering/rendering_primitives.cpp
@@ -19,151 +19,153 @@ namespace Poole::Rendering
 
 	//---
 
-	void StaticMeshNoIndiciesSolidColor3::Init()
-	{
-		GLuint vertexArrayID;
-		glGenVertexArrays(1, &vertexArrayID);
-		glBindVertexArray(vertexArrayID);
-
-		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
-		glGenBuffers(1, &m_vertexbuffer);
-		//The following commands will talk about our 'm_vertexbuffer' buffer
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-		//Give our vertices to OpenGL.
-		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
-	}
-	void StaticMeshNoIndiciesSolidColor3::Render(GLuint programId)
-	{
-		//Use Shader
-		glUseProgram(programId);
-
-		SetUniforms(programId);
-		//GLint Loc = glGetUniformLocation(programId, "uniformColor");
-		//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-
-		//1st attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(
-			0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
-			sizeof(TVertex) / sizeof(f32),  //size
-			GL_FLOAT,						//type
-			GL_FALSE,						//normalized?
-			3 * sizeof(f32),				//stride
-			(void*)0						//array buffer offset
-		);
-		//Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, (GLsizei)m_verts.size()); //Starting from vertex 0; N vertices total.
-		glDisableVertexAttribArray(0);
-	}
-
-	//---
-
-	void StaticMeshSolidColor3::Init()
-	{
-		GLuint vertexArrayID;
-		glGenVertexArrays(1, &vertexArrayID);
-		glBindVertexArray(vertexArrayID);
-		
-		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
-		glGenBuffers(1, &m_vertexbuffer);
-		//The following commands will talk about our 'm_vertexbuffer' buffer
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-		//Give our vertices to OpenGL.
-		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
-		
-		//Generate 1 buffer, put the resulting identifier in m_elementbuffer
-		glGenBuffers(1, &m_elementbuffer);
-		//The following commands will talk about our 'm_elementbuffer' buffer
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
-		//Give our indices to OpenGL.
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
-	}
-	void StaticMeshSolidColor3::Render(GLuint programId)
-	{
-		//Use Shader
-		glUseProgram(programId);
-
-		SetUniforms(programId);
-		//GLint Loc = glGetUniformLocation(programId, "uniformColor");
-		//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
-
-		//1st attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(
-			0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
-			sizeof(TVertex) / sizeof(f32),  //size
-			GL_FLOAT,						//type
-			GL_FALSE,						//normalized?
-			0,								//stride
-			(void*)0						//array buffer offset
-		);
-		//Draw the triangle !
-		glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
-		glDisableVertexAttribArray(0);
-	}
-
-	//--
-
-	void StaticMeshVertexColor3::Init()
-	{
-		GLuint vertexArrayID;
-		glGenVertexArrays(1, &vertexArrayID);
-		glBindVertexArray(vertexArrayID);
-
-		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
-		glGenBuffers(1, &m_vertexbuffer);
-		//The following commands will talk about our 'm_vertexbuffer' buffer
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-		//Give our vertices to OpenGL.
-		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
-
-		//Generate 1 buffer, put the resulting identifier in m_elementbuffer
-		glGenBuffers(1, &m_elementbuffer);
-		//The following commands will talk about our 'm_elementbuffer' buffer
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
-		//Give our vertices to OpenGL.
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
-	}
-	void StaticMeshVertexColor3::Render(GLuint programId)
-	{
-		//Use Shader
-		glUseProgram(programId);
-
-		SetUniforms(programId);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
-
-		//0th attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(
-			0,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
-			sizeof(TVertex::TVertexPart) / sizeof(f32), //size
-			GL_FLOAT,									//type
-			GL_FALSE,									//normalized?
-			sizeof(TVertex),							//stride
-			(void*)0									//array buffer offset
-		);
-
-		//1st attribute buffer : color
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(
-			1,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
-			sizeof(TVertex::TColorPart) / sizeof(f32),	//size
-			GL_FLOAT,									//type
-			GL_FALSE,									//normalized?
-			sizeof(TVertex),							//stride
-			(char*)(sizeof(TVertex::TVertexPart))		//array buffer offset
-		);
-
-		//Draw the triangle !
-		glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-	}
+	//template<class ... TDecorators>
+	//void StaticMeshNoIndiciesSolidColor3<TDecorators...>::Init()
+	//{
+	//	GLuint vertexArrayID;
+	//	glGenVertexArrays(1, &vertexArrayID);
+	//	glBindVertexArray(vertexArrayID);
+	//
+	//	//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+	//	glGenBuffers(1, &m_vertexbuffer);
+	//	//The following commands will talk about our 'm_vertexbuffer' buffer
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//	//Give our vertices to OpenGL.
+	//	glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+	//}
+	//template<class ... TDecorators>
+	//void StaticMeshNoIndiciesSolidColor3<TDecorators...>::Render(GLuint programId)
+	//{
+	//	//Use Shader
+	//	glUseProgram(programId);
+	//
+	//	SetUniforms(programId);
+	//	//GLint Loc = glGetUniformLocation(programId, "uniformColor");
+	//	//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
+	//
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//
+	//	//1st attribute buffer : vertices
+	//	glEnableVertexAttribArray(0);
+	//	glVertexAttribPointer(
+	//		0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
+	//		sizeof(TVertex) / sizeof(f32),  //size
+	//		GL_FLOAT,						//type
+	//		GL_FALSE,						//normalized?
+	//		3 * sizeof(f32),				//stride
+	//		(void*)0						//array buffer offset
+	//	);
+	//	//Draw the triangle !
+	//	glDrawArrays(GL_TRIANGLES, 0, (GLsizei)m_verts.size()); //Starting from vertex 0; N vertices total.
+	//	glDisableVertexAttribArray(0);
+	//}
+	//
+	////---
+	//
+	//void StaticMeshSolidColor3::Init()
+	//{
+	//	GLuint vertexArrayID;
+	//	glGenVertexArrays(1, &vertexArrayID);
+	//	glBindVertexArray(vertexArrayID);
+	//	
+	//	//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+	//	glGenBuffers(1, &m_vertexbuffer);
+	//	//The following commands will talk about our 'm_vertexbuffer' buffer
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//	//Give our vertices to OpenGL.
+	//	glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+	//	
+	//	//Generate 1 buffer, put the resulting identifier in m_elementbuffer
+	//	glGenBuffers(1, &m_elementbuffer);
+	//	//The following commands will talk about our 'm_elementbuffer' buffer
+	//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+	//	//Give our indices to OpenGL.
+	//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
+	//}
+	//void StaticMeshSolidColor3::Render(GLuint programId)
+	//{
+	//	//Use Shader
+	//	glUseProgram(programId);
+	//
+	//	SetUniforms(programId);
+	//	//GLint Loc = glGetUniformLocation(programId, "uniformColor");
+	//	//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
+	//
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+	//
+	//	//1st attribute buffer : vertices
+	//	glEnableVertexAttribArray(0);
+	//	glVertexAttribPointer(
+	//		0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
+	//		sizeof(TVertex) / sizeof(f32),  //size
+	//		GL_FLOAT,						//type
+	//		GL_FALSE,						//normalized?
+	//		0,								//stride
+	//		(void*)0						//array buffer offset
+	//	);
+	//	//Draw the triangle !
+	//	glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
+	//	glDisableVertexAttribArray(0);
+	//}
+	//
+	////--
+	//
+	//void StaticMeshVertexColor3::Init()
+	//{
+	//	GLuint vertexArrayID;
+	//	glGenVertexArrays(1, &vertexArrayID);
+	//	glBindVertexArray(vertexArrayID);
+	//
+	//	//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+	//	glGenBuffers(1, &m_vertexbuffer);
+	//	//The following commands will talk about our 'm_vertexbuffer' buffer
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//	//Give our vertices to OpenGL.
+	//	glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+	//
+	//	//Generate 1 buffer, put the resulting identifier in m_elementbuffer
+	//	glGenBuffers(1, &m_elementbuffer);
+	//	//The following commands will talk about our 'm_elementbuffer' buffer
+	//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+	//	//Give our vertices to OpenGL.
+	//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
+	//}
+	//void StaticMeshVertexColor3::Render(GLuint programId)
+	//{
+	//	//Use Shader
+	//	glUseProgram(programId);
+	//
+	//	SetUniforms(programId);
+	//
+	//	glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+	//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+	//
+	//	//0th attribute buffer : vertices
+	//	glEnableVertexAttribArray(0);
+	//	glVertexAttribPointer(
+	//		0,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
+	//		sizeof(TVertex::TVertexPart) / sizeof(f32), //size
+	//		GL_FLOAT,									//type
+	//		GL_FALSE,									//normalized?
+	//		sizeof(TVertex),							//stride
+	//		(void*)0									//array buffer offset
+	//	);
+	//
+	//	//1st attribute buffer : color
+	//	glEnableVertexAttribArray(1);
+	//	glVertexAttribPointer(
+	//		1,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
+	//		sizeof(TVertex::TColorPart) / sizeof(f32),	//size
+	//		GL_FLOAT,									//type
+	//		GL_FALSE,									//normalized?
+	//		sizeof(TVertex),							//stride
+	//		(char*)(sizeof(TVertex::TVertexPart))		//array buffer offset
+	//	);
+	//
+	//	//Draw the triangle !
+	//	glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
+	//	glDisableVertexAttribArray(0);
+	//	glDisableVertexAttribArray(1);
+	//}
 }
--- src/rendering/rendering_primitives.h
+++ src/rendering/rendering_primitives.h
@@ -10,7 +10,7 @@ namespace Poole::Rendering
 
 #pragma warning(disable : 4201) //DISABLE nameless struct/union
 	template<typename _TVertexPart, typename _TColorPart>
-	struct VertexWithColor{ };
+	struct VertexWithColor { };
 
 	template<>
 	struct VertexWithColor<Vertex, fcolor3>
@@ -78,7 +78,7 @@ namespace Poole::Rendering
 		virtual void SetInternalUniforms(const GLuint programId) { }
 	protected:
 		template<typename TFloats> void SetUniform(const GLint uniform, const TFloats f) { assert(0); }
-		template<> void SetUniform(const GLint uniform, const f32 f)   { glUniform1f(uniform, f); }
+		template<> void SetUniform(const GLint uniform, const f32 f) { glUniform1f(uniform, f); }
 		template<> void SetUniform(const GLint uniform, const fvec2 v) { glUniform2f(uniform, v.x, v.y); }
 		template<> void SetUniform(const GLint uniform, const fvec3 v) { glUniform3f(uniform, v.x, v.y, v.z); }
 		template<> void SetUniform(const GLint uniform, const fvec4 v) { glUniform4f(uniform, v.x, v.y, v.z, v.w); }
@@ -99,13 +99,13 @@ namespace Poole::Rendering
 		T MemberName;																\
 	};
 
-	MakeUniform(SolidColor,		 "uniformColor",	m_color);
+	MakeUniform(SolidColor, "uniformColor", m_color);
 	MakeUniform(DynamicPosition, "uniformPosition", m_position);
 	MakeUniform(DynamicRotation, "uniformRotation", m_rotation);
-	MakeUniform(DynamicScale,	 "uniformScale",	m_scale);
+	MakeUniform(DynamicScale, "uniformScale", m_scale);
 
 
-	#define MeshUniformCollection_2DTransform \
+#define MeshUniformCollection_2DTransform \
 		  MeshUniform_DynamicPosition<fvec2> \
 		, MeshUniform_DynamicRotation<f32> \
 		, MeshUniform_DynamicScale<fvec2>
@@ -122,11 +122,11 @@ namespace Poole::Rendering
 	};
 
 	template<class ... TDecorators>
-	struct IMeshUniformCollector 
+	struct IMeshUniformCollector
 		: public IMeshUniformCollectorBase
 		, public TDecorators...
 	{
-		static_assert((std::is_base_of<IMeshUniformDataBase, TDecorators>::value && ...), 
+		static_assert((std::is_base_of<IMeshUniformDataBase, TDecorators>::value && ...),
 			"All TDecorators must inherit from IMeshUniformDataBase struct.");
 
 		virtual void SetAllUniforms(GLuint programId) override
@@ -172,38 +172,212 @@ namespace Poole::Rendering
 
 
 
-
+	template<class ... TDecorators>
 	struct StaticMeshNoIndiciesSolidColor3
 		: public IMesh
 		, public MeshFeature_HasVerts<Vertex>
-		, public IMeshUniformCollector<MeshUniform_SolidColor<fcolor3>>
+		, public IMeshUniformCollector<MeshUniform_SolidColor<fcolor3>, TDecorators...>
 	{
 		virtual void Init() override;
 		virtual void Render(GLuint programId) override;
 	};
 
+	struct StaticMeshNoIndiciesSolidColor3_2DTransform
+		: public StaticMeshNoIndiciesSolidColor3<MeshUniformCollection_2DTransform>
+	{
+	};
 
 
+	template<class ... TDecorators>
 	struct StaticMeshSolidColor3
 		: public IMesh
 		, public MeshFeature_HasVerts<Vertex>
 		, public MeshFeature_HasIndicies
-		, public IMeshUniformCollector< MeshUniform_SolidColor<fcolor3> >
+		, public IMeshUniformCollector<MeshUniform_SolidColor<fcolor3>, TDecorators...>
 	{
 		virtual void Init() override;
 		virtual void Render(GLuint programId) override;
 	};
 
+	struct StaticMeshSolidColor3_2DTransform
+		: public StaticMeshSolidColor3<MeshUniformCollection_2DTransform>
+	{
+	};
 
 
+	template<class ... TDecorators>
 	struct StaticMeshVertexColor3
 		: public IMesh
 		, public MeshFeature_HasVerts<VertexWithColor3>
 		, public MeshFeature_HasIndicies
+		, public IMeshUniformCollector<TDecorators...>
 	{
 		virtual void Init() override;
 		virtual void Render(GLuint programId) override;
 	};
+
+	struct StaticMeshVertexColor3_2DTransform
+		: public StaticMeshVertexColor3<MeshUniformCollection_2DTransform>
+	{
+	};
+
+
+
+
+
+	template<class ... TDecorators>
+	void StaticMeshNoIndiciesSolidColor3<TDecorators...>::Init()
+	{
+		GLuint vertexArrayID;
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+		glGenBuffers(1, &m_vertexbuffer);
+		//The following commands will talk about our 'm_vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+		//Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+	}
+	template<class ... TDecorators>
+	void StaticMeshNoIndiciesSolidColor3<TDecorators...>::Render(GLuint programId)
+	{
+		//Use Shader
+		glUseProgram(programId);
+
+		SetUniforms(programId);
+		//GLint Loc = glGetUniformLocation(programId, "uniformColor");
+		//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+
+		//1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
+			sizeof(TVertex) / sizeof(f32),  //size
+			GL_FLOAT,						//type
+			GL_FALSE,						//normalized?
+			3 * sizeof(f32),				//stride
+			(void*)0						//array buffer offset
+		);
+		//Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, (GLsizei)m_verts.size()); //Starting from vertex 0; N vertices total.
+		glDisableVertexAttribArray(0);
+	}
+
+
+	//---
+
+	template<class ... TDecorators>
+	void StaticMeshSolidColor3<TDecorators...>::Init()
+	{
+		GLuint vertexArrayID;
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+		glGenBuffers(1, &m_vertexbuffer);
+		//The following commands will talk about our 'm_vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+		//Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+
+		//Generate 1 buffer, put the resulting identifier in m_elementbuffer
+		glGenBuffers(1, &m_elementbuffer);
+		//The following commands will talk about our 'm_elementbuffer' buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+		//Give our indices to OpenGL.
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
+	}
+	template<class ... TDecorators>
+	void StaticMeshSolidColor3<TDecorators...>::Render(GLuint programId)
+	{
+		//Use Shader
+		glUseProgram(programId);
+
+		SetUniforms(programId);
+		//GLint Loc = glGetUniformLocation(programId, "uniformColor");
+		//glUniform3f(Loc, m_color.r, m_color.g, m_color.b);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+
+		//1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,								//attribute 0. No particular reason for 0, but must match the layout in the shader.
+			sizeof(TVertex) / sizeof(f32),  //size
+			GL_FLOAT,						//type
+			GL_FALSE,						//normalized?
+			0,								//stride
+			(void*)0						//array buffer offset
+		);
+		//Draw the triangle !
+		glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
+		glDisableVertexAttribArray(0);
+	}
+
+	//--
+
+	template<class ... TDecorators>
+	void StaticMeshVertexColor3<TDecorators...>::Init()
+	{
+		GLuint vertexArrayID;
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		//Generate 1 buffer, put the resulting identifier in m_vertexbuffer
+		glGenBuffers(1, &m_vertexbuffer);
+		//The following commands will talk about our 'm_vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+		//Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(TVertex) * m_verts.size(), m_verts.data(), GL_STATIC_DRAW);
+
+		//Generate 1 buffer, put the resulting identifier in m_elementbuffer
+		glGenBuffers(1, &m_elementbuffer);
+		//The following commands will talk about our 'm_elementbuffer' buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+		//Give our vertices to OpenGL.
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
+	}
+	template<class ... TDecorators>
+	void StaticMeshVertexColor3<TDecorators...>::Render(GLuint programId)
+	{
+		//Use Shader
+		glUseProgram(programId);
+
+		SetUniforms(programId);
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexbuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_elementbuffer);
+
+		//0th attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
+			sizeof(TVertex::TVertexPart) / sizeof(f32), //size
+			GL_FLOAT,									//type
+			GL_FALSE,									//normalized?
+			sizeof(TVertex),							//stride
+			(void*)0									//array buffer offset
+		);
+
+		//1st attribute buffer : color
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(
+			1,											//attribute 0. No particular reason for 0, but must match the layout in the shader.
+			sizeof(TVertex::TColorPart) / sizeof(f32),	//size
+			GL_FLOAT,									//type
+			GL_FALSE,									//normalized?
+			sizeof(TVertex),							//stride
+			(char*)(sizeof(TVertex::TVertexPart))		//array buffer offset
+		);
+
+		//Draw the triangle !
+		glDrawElements(GL_TRIANGLES, (GLsizei)m_indices.size(), GL_UNSIGNED_INT, 0);
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+	}
+
 }
-
-
